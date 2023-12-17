7c99384c61587c88fc3f7c1dacaf804c30e3532c
CocoWu <coco18810928062@gmail.com>
2023-12-18 00:50:43 +0800

    DAY6

--- Week3/Day6/DailyChallenge/java.js
+++ Week3/Day6/DailyChallenge/java.js
@@ -1,1 +1,16 @@
-console.log("Hi!")
+// single loop
+let star = "";
+let starline = 6; 
+for (let i = 1; i <= starline; i++) {
+    star += "* ".repeat(i)+ "\n" ;
+}
+console.log(star);
+
+// nested loop
+for(let i = 0; i <= 6; i++){
+    let star = " "
+    for(let x=0; x<=i;x++){
+        star = star + "* "
+    }
+    console.log(star)
+}
